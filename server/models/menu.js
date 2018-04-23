923520a5493ee602aef07a2de4bfe645c69d9ee4
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-04-23 07:58:44 -1200

    add models file for dummy data

---
+++ server/models/menu.js
@@ -0,0 +1,48 @@
+const menu = [
+    {
+        date: "16-09-2018",
+        meals: [
+            {
+                id: 1,
+                food: 'Jollof-rice',
+                quantity: 1,
+                image: "img.png",
+                amount: "5000",
+                category: "local-dish"
+            },
+            {
+                id: 2,
+                food: 'Amala',
+                quantity: 2,
+                image: "img.png",
+                amount: "10000",
+                category: "local-dish"
+            },
+            {
+                id: 3,
+                food: 'ofada rice',
+                quantity: 1,
+                image: "img.png",
+                amount: "15000",
+                category: "local-dish",
+            },
+            {
+                id: 4,
+                food: 'salad',
+                quantity: 1,
+                image: "img.png",
+                amount: "5000",
+                category: "intercontinental-dish"
+            },
+            {
+                id: 5,
+                food: 'chicken',
+                quantity: 2,
+                image: "img.png",
+                amount: "10000",
+                category: "intercontinental-dish",
+            }
+        ]
+    }
+]
+ export default menu;
---
+++ server/models/order.js
@@ -0,0 +1,22 @@
+const order = [
+    {
+        id: 1,
+        user: "Ade",
+        food: "Amala",
+        amount: 12000
+    },
+    {
+        id: 2,
+        user: "John",
+        food: "Jollof",
+        amount: 10000
+    },
+    {
+        id: 3,
+        user: "Segun",
+        food: "Ofada",
+        amount: 15000
+    },
+]
+
+export default order;

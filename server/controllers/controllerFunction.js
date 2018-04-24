a37acab246a888afe3245ad703724109ec5c54ee
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-04-24 08:01:24 -1200

    modifiy files

--- server/controllers/controllerFunction.js
+++ server/controllers/controllerFunction.js
@@ -12,44 +12,6 @@ const getAll = (element, req, res) => {
   });
 };
 
-const add = (element, req, res) => {
-  for (let i = 0; i < element.length; i += 1) {
-    if (req.body.id === element[i].id) {
-      return res.json({
-        message: "the 'id' already existing",
-        error: true
-      });
-    } else if (!req.body.id) {
-      return res.json({
-        message: "the 'id' is required",
-        error: true
-      });
-    }
-  }
-  element.push(req.body);
-  return res.json({
-    message: 'Success',
-    error: false
-  });
-};
-
-const update = (element, req, res) => {
-  for (let i = 0; i < element.length; i += 1) {
-    if (element[i].id === parseInt(req.params.id, 10)) {
-      element[i].name = req.body.name;
-      element[i].cost = req.body.cost;
-      return res.json({
-        message: 'Update Successful',
-        error: false
-      });
-    }
-  }
-  return res.status(404).json({
-    message: 'not found',
-    error: true
-  });
-};
-
 const remove = (element, req, res) => {
   for (let i = 0; i < element.length; i += 1) {
     if (element[i].id === parseInt(req.params.id, 10)) {
@@ -104,5 +66,5 @@ const getByGroup = (element, req, res) => {
 
 
 export default {
-  getAll, add, update, remove, getById, getByGroup
+  getAll, remove, getById, getByGroup
 };

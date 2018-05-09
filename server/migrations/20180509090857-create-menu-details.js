3e4524ad782682a696cd5240204224fc06b39226
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-09 01:24:58 -1200

    add the migration the join table

---
+++ server/migrations/20180509090857-create-menu-details.js
@@ -0,0 +1,29 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('menuDetails', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    mealId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    menuId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('menuDetails')
+};

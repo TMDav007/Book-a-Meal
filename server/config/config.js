2210a5d734960ef0ff55266a16edd0ff366af87a
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-10 20:23:38 -1200

    modify file

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   production: {
     use_env_variable: 'postgres://pkygpiytzhylqc:ea73f35f464e42d400aefe6be4bc45d4a2f5579fc682a5f34be91d6325be9a93@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d7d5gff5hjp7qe',
+    url: 'postgres://pkygpiytzhylqc:ea73f35f464e42d400aefe6be4bc45d4a2f5579fc682a5f34be91d6325be9a93@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d7d5gff5hjp7qe',
     dialect: 'postgres'
   }
 };
